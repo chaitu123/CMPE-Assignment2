010cff3b4e3fad141068b43f59b35746acbe1e63
Poojitha Srinivasa <clusp.p@gmail.com>
2013-11-04 00:45:19 -0800

    test

---
+++ library/src/main/java/edu/sjsu/cmpe/library/LibraryService.java
@@ -0,0 +1,62 @@
+package edu.sjsu.cmpe.library;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.yammer.dropwizard.Service;
+//import com.yammer.dropwizard.assets.AssetsBundle;
+import com.yammer.dropwizard.config.Bootstrap;
+import com.yammer.dropwizard.config.Environment;
+import com.yammer.dropwizard.views.ViewBundle;
+
+import edu.sjsu.cmpe.library.api.resources.BookResource;
+import edu.sjsu.cmpe.library.api.resources.RootResource;
+import edu.sjsu.cmpe.library.config.LibraryServiceConfiguration;
+import edu.sjsu.cmpe.library.repository.BookRepository;
+import edu.sjsu.cmpe.library.repository.BookRepositoryInterface;
+import edu.sjsu.cmpe.library.ui.resources.HomeResource;
+
+public class LibraryService extends Service<LibraryServiceConfiguration> {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    public static void main(String[] args) throws Exception {
+	new LibraryService().run(args);
+    }
+
+    @Override
+    public void initialize(Bootstrap<LibraryServiceConfiguration> bootstrap) {
+	bootstrap.setName("library-service");
+	bootstrap.addBundle(new ViewBundle());
+	//bootstrap.addBundle(new AssetsBundle());
+    }
+
+    @Override
+    public void run(LibraryServiceConfiguration configuration,
+	    Environment environment) throws Exception {
+	// This is how you pull the configurations from library_x_config.yml
+	String queueName = configuration.getStompQueueName();
+	String topicName = configuration.getStompTopicName();
+	log.debug("{} - Queue name is {}. Topic name is {}",
+		configuration.getLibraryName(), queueName,
+		topicName);
+	// TODO: Apollo STOMP Broker URL and login
+	String apollouser=configuration.getApolloUser();
+	String apolloPassword=configuration.getApolloPassword();
+	String apollohost=configuration.getApolloHost();
+	int apolloPort=configuration.getApolloPort();
+	 
+	log.debug(log+"\nApollo User: "+apolloUser+"\nApollo Password: "+apolloPassword+"\nApollo Host: "+ apolloHost+"\nApollo Port: "+apolloPort);
+	/** Root API */
+	environment.addResource(RootResource.class);
+	/** Books APIs */
+	BookRepositoryInterface bookRepository = new BookRepository();
+	environment.addResource(new BookResource(bookRepository));
+
+	/** UI Resources */
+	environment.addResource(new HomeResource(bookRepository));
+	
+	//environment.addResource(new BookResource(bookRepository,bookrepoactions));
+	//environment.addResource(new HomeResource(bookRepository));
+    }
+}
